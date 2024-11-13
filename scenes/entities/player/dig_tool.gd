fe312b537ec22cd5a8397e60ec3ba79cb67897de
Pedro <pedrosteinmacher@gmail.com>
2024-11-13 16:06:54 -0300

    arrumando animacao da pika

--- scenes/entities/player/dig_tool.gd
+++ scenes/entities/player/dig_tool.gd
@@ -2,9 +2,16 @@ extends Node2D
 
 @onready var ANIMATION_PLAYER: AnimationPlayer = $AnimationPlayer;
 
+var toggle_animation = -1
+
 func _get_input():
 	#Handles the "DIG" tool position
 	var mouse_position: Vector2 = get_global_mouse_position()
+	
+	scale.x = 1 * toggle_animation
+	if global_position.x > mouse_position.x and not ANIMATION_PLAYER.is_playing():
+		scale.x = -1 * toggle_animation
+	
 	look_at(mouse_position);
 	rotate(PI/2)
 	
@@ -12,6 +19,9 @@ func _get_input():
 		dig()
 
 func dig():
+	if ANIMATION_PLAYER.is_playing():
+		return
+	toggle_animation *= -1
 	ANIMATION_PLAYER.play("digging")
 
 func _physics_process(delta):
