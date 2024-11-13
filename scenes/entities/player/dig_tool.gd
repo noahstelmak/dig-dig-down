26d1276f4b2232beeca98ccf180f20c553af568c
noahstelmak <noah.bussmann@gmail.com>
2024-11-13 13:37:42 -0300

    mexi na estrutura

--- scenes/entities/player/dig_tool.gd
+++ scenes/entities/player/dig_tool.gd
@@ -1,31 +1,18 @@
-extends Area2D
+extends Node2D
 
-@onready var PLAYER = $"../../Player";
-@onready var ANIMATION_PLAYER = $AnimationPlayer;
-@onready var NODE2D = $"..";
-
-var digging: bool = false;
+@onready var ANIMATION_PLAYER: AnimationPlayer = $AnimationPlayer;
 
 func _get_input():
-	#Handles the "DIG" tool
+	#Handles the "DIG" tool position
 	var mouse_position: Vector2 = get_global_mouse_position()
-	NODE2D.global_position = PLAYER.global_position.direction_to(mouse_position) * PLAYER.SWING_RANGE + PLAYER.global_position
-	NODE2D.look_at(mouse_position);
+	look_at(mouse_position);
+	rotate(PI/2)
 	
-	if PLAYER.global_position.distance_to(mouse_position) > PLAYER.SWING_RANGE:
-		NODE2D.rotate(PI/2);
-	else:
-		NODE2D.rotate(-PI/2);
-
-	# Handles the "DIG" action key 
 	if Input.is_action_just_pressed("dig"):
 		dig()
 
 func dig():
-	digging = true;
 	ANIMATION_PLAYER.play("digging")
-	digging = false;
 
 func _physics_process(delta):
 	_get_input();
-		
