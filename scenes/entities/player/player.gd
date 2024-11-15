560fc54b51c46c62efdd00aa0bba5a3bcc2040d9
noahstelmak <noah.bussmann@gmail.com>
2024-11-14 23:18:34 -0300

    Player Animations

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -8,12 +8,50 @@ extends CharacterBody2D
 func _ready():
 	pass
 
-# Handles input
 func get_input():
-	# Handles movement
 	var direction: Vector2 = Input.get_vector('left', 'right', 'up', 'down');
-	velocity = direction * SPEED;
+	play_animations(direction)
+	handle_movement(direction)
 
-func _physics_process(delta):
+func play_animations(direction: Vector2):
+	if direction.length() == 0:
+		var angle = get_local_mouse_position().angle()
+		if angle > -PI / 4 and angle <= PI / 4:
+			$DigTool.z_index = 1
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("idle_side")
+		elif angle > PI / 4 and angle <= 3 * PI / 4:
+			$DigTool.z_index = 1
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("idle_front")
+		elif angle > -3 * PI / 4 and angle <= -PI / 4:
+			$DigTool.z_index = -1
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("idle_back")
+		else:
+			$DigTool.z_index = 1
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("idle_side")
+	elif direction.x > 0:
+		$DigTool.z_index = 1
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("walk_side");
+	elif direction.x < 0:
+		$DigTool.z_index = 1
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("walk_side");
+	elif direction.y > 0:
+		$DigTool.z_index = 1
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("walk_front");
+	else:
+		$DigTool.z_index = 1
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("walk_back");
+
+func handle_movement(direction: Vector2):
+	velocity = direction * SPEED;
+	
+func _physics_process(_delta):
 	get_input();
 	move_and_slide();
