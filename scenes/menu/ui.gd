1ee67d074297dc815283d6cc3dbba0387a939116
noahstelmak <noah.bussmann@gmail.com>
2024-11-15 17:20:15 -0300

    Health Container

--- scenes/menu/ui.gd
+++ scenes/menu/ui.gd
@@ -1,6 +1,11 @@
 extends Control
  
 @onready var coinCounter = $CoinCounter;
+@onready var healthContainer = $HealthContainer;
 
 func _process(_delta):
-	coinCounter.text = str(Globals.COIN_ORE_COUNT)
+	coinCounter.text = str(Globals.COIN_ORE_COUNT);
+	
+	for i in range(5):
+		var child = healthContainer.get_child(i)
+		child.visible = i < Globals.PLAYER_HEALTH
