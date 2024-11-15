8df4601b90b43932edffcec81a6062dae43e2e05
Pedro <pedrosteinmacher@gmail.com>
2024-11-15 18:33:08 -0300

    intro fix

--- assets/intro.gd
+++ assets/intro.gd
@@ -1,12 +1,15 @@
 extends CanvasLayer
 
+var scene = preload("res://scenes/menu/main_menu/main_menu.tscn").instantiate()
+
 func _ready():
 	$AnimationPlayer.play("prisma")
 	$AudioStreamPlayer.play()
 	await get_tree().create_timer(15).timeout
-	get_tree().change_scene("res://ux/Main menu/Menu.tscn")
-
+	#get_tree().change_scene("res://scenes/menu/main_menu/main_menu.tscn")
+	get_tree().current_scene = scene
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("escape"):
-		get_tree().change_scene("res://ux/Main menu/Menu.tscn")
+		#get_tree().change_scene("res://scenes/menu/main_menu/main_menu.tscn")
+		get_tree().current_scene = scene
