7545fd5c1b038945ee192cfbd8a5ab9b54fc97d5
bruno.lopes <95176651+koddoki@users.noreply.github.com>
2024-11-15 22:54:46 -0300

    Update main.gd

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -9,6 +9,18 @@ extends Node
 @onready var floor_manager := preload("res://scenes/floors/floor_manager/floor_manager.tscn")
 
 
+func player_morreu():
+	var children := get_children()
+	for child in children:
+		child.queue_free()
+	
+	var main_menu_scene := load("res://scenes/menu/main_menu/main_menu.tscn")
+	var main_menu_instance = main_menu_scene.instantiate()
+	
+	add_child(main_menu_instance)
+	
+
+
 func _on_main_menu_start_game() -> void:
 	main_menu.queue_free()
 	var floor_manager_instance := floor_manager.instantiate()
